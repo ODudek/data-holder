88e52f288183f9c1faaff56986512adc9b80735a
ODudek <dudek.olek4@gmail.com>
2018-08-22 12:41:29 +0200

    schema fix

--- src/models/photoSchema.ts
+++ src/models/photoSchema.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const PhotoSchema = new Schema({
-	id: String,
+	photoId: String,
 	photoUrl: String,
 	thumbUrl: String,
 	title: String,
--- src/models/postSchema.ts
+++ src/models/postSchema.ts
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 export const PostSchema = new Schema({
 	content: String,
+	postId: String,
 	title: String,
 	userId: String,
 });
